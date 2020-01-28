c6eb888579da5ea0c8d01045f3cd6f48e7c1351a
Alper Yeniyayla <ayeniyayla@hotmail.de>
2020-01-28 17:24:52 +0100

    AsDocs2XML development started
    + Duplicated Angelscript Syntax in C# (Angelscript.cs)
    + Added ALREADY CORRECTED asdocs for various scripts into the folder Corrected Docs

---
+++ AsDocs2XML/Angelscript.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsDocs2XML
+{  
+    /*
+     * Mirroring the structure of the Angelscript scripts inside AsDocs2XML
+     * 
+     * We are using SortedLists to sort everything while parsing the files so we don't need to deal with
+     * stupid XML sorting routines later when we are generating the Database.xml file! :)
+     * 
+     * Since some elements will be sorted by their name we don't actually need to save the name explicity
+     * inside the element itself -- but we will do that anyway for completeness and uniformities sake.
+     * 
+     */
+    class ASScript
+    {
+        public string name;
+
+        public SortedList<string, ASClass> classes;
+        public SortedList<string, ASEnumeration> enumerations;
+        public SortedList<string, ASFunction> functions;
+        public SortedList<string, ASVariable> variables;
+
+        public ASScript(string name, string[] script)
+        {
+            this.name = name;
+            this.parseScript(script);
+        }
+
+        private void parseScript(string[] script)
+        {
+            // Do the magic
+        }
+    }
+
+    class ASClass
+    {
+        public string name;
+        public SortedList<string, ASClass> classes;
+        public SortedList<string, ASEnumeration> enumerations;
+        public SortedList<string, ASVariable> variables;
+
+        public ASClass(string[] script, int line)
+        {
+            this.parseClass(script, line);
+        }
+
+        private void parseClass(string[] script, int line)
+        {
+            // Do the magic 2
+        }
+    }
+
+    struct ASFunction
+    {
+        /*
+         *  void RibbonItemSetEnabled(const string &in, bool);
+         *  vec3 opMul(const vec3 &in) const;
+         *  const string &GetString (const string &in key);
+         *  const float &opIndex(uint) const;
+         *  JSON& opAssign(const JSON &in other);
+         */
+        public string name;
+        public List<ASOverload> overloads;            
+    }
+
+    struct ASOverload
+    {
+        public bool isConst;
+        public string returnType;
+        public List<ASParameter> parameters;
+
+        //const string &GetString (const string &in key);
+        //const string &GetString (const string &in key, const string &out bla);
+    }
+
+    struct ASParameter
+    {
+        public bool isConst;
+        public string type;
+        public string name;
+
+        //const string &in key
+    }
+
+    struct ASEnumeration
+    {
+        public string name;
+
+        // Be careful! We are using the enum values as the key since we want to sort after the values
+        // and not the enum member names!
+        public SortedList<int, string> enumerationMembers;
+
+        /*enum MouseButton {
+            LEFT = 0,
+            MIDDLE = 1,
+            RIGHT = 2,
+            FOURTH = 3,
+            FIFTH = 4,
+            SIXTH = 5,
+            SEVENTH = 6,
+            EIGHT = 7,
+            NINTH = 8,
+            TENTH = 9,
+            TWELFTH = 10
+        };*/
+    }
+
+    struct ASVariable
+    {
+        public bool isConst;
+        public string type;
+        public string name;
+
+        //const int _sound_priority_very_high;
+    }
+}
--- AsDocs2XML/Program.cs
+++ AsDocs2XML/Program.cs
@@ -4,15 +4,91 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+using System.Windows.Forms;
+
 namespace AsDocs2XML
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine("AsDocs2XML - please load your named asdocs header files!");
+            Console.WriteLine("AsDocs2XML - please load your corrected asdocs header files!");
+
+            /*
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "Overgrowth Angelscript Header Documentaion Files|*.txt;*.h";
+            fd.Title = "Select your corrected asdocs header files";
+            fd.Multiselect = true;
+            
+            if (fd.ShowDialog() != DialogResult.OK) return;
+            */
+
+            string[] files = new string[]
+            {
+                "..\\..\\Corrected Docs\\Camera.txt",
+                "..\\..\\Corrected Docs\\Character.txt",
+                "..\\..\\Corrected Docs\\Hotspot.txt",
+                "..\\..\\Corrected Docs\\Level.txt",
+                "..\\..\\Corrected Docs\\Scriptable Campaign.txt",
+                "..\\..\\Corrected Docs\\Scriptable UI.txt",
+            };
+
+            SortedList<string, ASScript> database = new SortedList<string, ASScript>();
+
+            foreach (string file in files)
+            {
+                string scriptName = Path.GetFileNameWithoutExtension(file).Replace(" ", "");
+                string[] scriptLines = File.ReadAllLines(file);
+
+                ASScript currentScript = new ASScript(scriptName, scriptLines);
+
+                database.Add(currentScript.name, currentScript);
+            }
+
+            foreach (ASScript currentScript in database.Values)
+            {
+                Console.WriteLine(currentScript.name);
+            }
 
             Console.ReadLine();
+
+
+
+            /*
+            static Regex regExFunction = new Regex(@"^\s*((const)?\s*([^,\s]+?)\s*([^,\s]+?)\s*\(([^\)]*?)\)\s*(const)?).*?$");
+            static Regex regExVariable = new Regex(@"^\s*(const)?\s*([^,\s}]+?)\s*([^,\s*\(}]+?)\s*$");
+            static Regex regExClass = new Regex(@"^\s*class\s*(.+?)\s*{\s*.*?$");
+            static Regex regExEnum = new Regex(@"^\s*enum\s*(.+?)\s*{\s*.*?$");
+
+            static Regex regExParams = new Regex(@"\s*((?:[^,\s]+\s*)+)\s*,?");
+            static Regex regExEnumValue = new Regex(@"^\s*([^,\s=]+)\s*=\s*-?(\d+)\s*,?\s*$");
+            */
+
+            #region Verfahren
+            /* 
+             * Datei zeilenweise durchgehen?
+             * Klasse gefunden?
+             * Existiert currentRootNode.<Classes>? Wenn nicht erstellen
+             *    currentRootNode.<Classes>.<vec3> hinzufügen
+             *    appendScriptcontent(currentRootNode.<Classes>.<vec3>, script, begin = currentLine + 1)
+             *      ^-> macht sein Ding und passt currentLine an. Die Parent Funktion wird dann bei der nächsten Iteration
+             *          nicht mehr in der Klasse sein, weil currentLine angepasst wurde :)
+             * 
+             *    Variable gefunden?
+             *        Existiert currentRootNode.<Variables>? Wenn nicht erstellen
+             *        geparste Variable einfügen
+             * 
+             *    Funktion gefunden?
+             *        Existiert currentRootNode.<Functions>? Wenn nicht erstellen
+             *        geparste Funktion als Overload einfügen
+             * 
+             *    Enum gefunden?
+             *       Existiert currentRootNode.<Enumerations>? Wenn nicht erstellen
+             *        geparstes Enum einfügen
+            */
+            #endregion
         }
     }
 }
