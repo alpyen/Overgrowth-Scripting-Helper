f13fab93be7427f789f0150325deb94951405e54
alpyen <53614168+alpyen@users.noreply.github.com>
2021-02-08 21:17:50 +0100

    Restart project work / Updating to latest dependencies.

---
+++ Overgrowth Scripting Helper/Forms/frmMyDlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Overgrowth_Scripting_Helper.NppPluginNET
+{
+    public partial class frmMyDlg : Form
+    {
+        public frmMyDlg()
+        {
+            InitializeComponent();
+        }
+    }
+}
